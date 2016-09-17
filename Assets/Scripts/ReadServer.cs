c7e8c9374cae6359cb3d758fa603d91439614c3c
David Tran <dt_dbqt@hotmail.com>
2016-09-17 09:37:47 -0400

    server read test ok

---
+++ Assets/Scripts/ReadServer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ReadServer : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+	   WWW w = new WWW("https://projectava-1de83.firebaseio.com/test.json");
+
+       yield return w;
+      
+      Debug.Log(w.text);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
