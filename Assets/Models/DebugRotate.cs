b6b939ed355e89e73da7816591c08bf7ee862b75
David Tran <dt_dbqt@hotmail.com>
2016-09-17 03:10:23 -0400

    cameras for projection done

---
+++ Assets/Models/DebugRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   this.gameObject.transform.Rotate(Vector3.up*10f*Time.deltaTime);
+	}
+}
